13ad2b5e63f6d0b544072a5e5076db344bf1664a
JavierCcaccyaAroni <javier_luis10@hotmail.com>
2025-10-12 23:14:19 -0500

    tratando de conectar a la DB

--- semana2/todolist-api/app/config.py
+++ semana2/todolist-api/app/config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = "customsecretkey"
-    SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost:3306/codigo_15_flask"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:password@127.0.0.1:3306/codigo_15_flask"
     SQLALCHEMY_TRACKS_MODIFICATIONS = False
